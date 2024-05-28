b6a82422c5a4271c1dfef04e4f65858e2a8d09e1
Bhimani Yash Shaileshbhai <161473598+YashGaara@users.noreply.github.com>
2024-05-28 08:16:40 +0530

    Create num.php

---
+++ num.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    echo 4;
+    echo "</br>";
+    echo 7.5;
+    echo "</br>";
+    echo 72+9;
+    echo "</br>";
+    echo 13-5;
+    echo "</br>";
+
+    ?>
+    
+</body>
+</html>
