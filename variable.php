d505842316c2175997c9ad23d4393250140e6033
Bhimani Yash Shaileshbhai <161473598+YashGaara@users.noreply.github.com>
2024-05-28 08:16:47 +0530

    Create variable.php

---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php
+$tadpole_age = 15;
+$silly_age=8;
+$age_addtion=$tadpole_age + $silly_age;
+$age_diffrence=$tadpole_age - $silly_age;
+echo $age_addtion;
+echo"</br>";
+echo $age_diffrence;
+echo"</br>";
+$favourite_anime=15;
+echo $favourite_anime+1;
+echo "</br>";
+echo $favourite_anime;
+echo"</br>";
+echo 4*3;
+echo "</br>";
+echo $favourite_anime;
+echo "</br>";
+
+
+?>
