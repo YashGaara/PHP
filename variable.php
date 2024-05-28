8232390dc38670443381468ee1d18c4b62e22c34
Bhimani Yash Shaileshbhai <161473598+YashGaara@users.noreply.github.com>
2024-05-28 16:26:23 +0530

    Update variable.php

--- variable.php
+++ variable.php
@@ -16,6 +16,22 @@ echo 4*3;
 echo "</br>";
 echo $favourite_anime;
 echo "</br>";
+echo 8**2;
+echo"</br>";
+echo 4**3;
+echo "</br>";
+echo 7%3;
+echo"</br>";
+echo 30%3;
+echo "</br>";
+$a=20;
+$b=8;
+$c=$a % $b;
+echo $c;
+echo"</br>";
+echo 2+5+7+30-2.9;
+echo"</br>";
+echo 3 + 7 + 6 - 7 / 5 % 2;
 
 
 ?>
